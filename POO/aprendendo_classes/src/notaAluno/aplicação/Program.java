e3eac3d08077dc9927969107dfbf3036549278cf
Conexa <pablogabriel1941@gmail.com>
2024-11-18 16:08:03 -0300

    .

---
+++ POO/aprendendo_classes/src/notaAluno/aplicação/Program.java
@@ -0,0 +1,14 @@
+package notaAluno.aplicação;
+
+import notaAluno.entidade.Student;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Student teste = new Student();
+
+        System.out.printf("%.2f%n", teste.somaSimestre(100,100,100));
+
+    }
+}
---
+++ POO/aprendendo_classes/src/notaAluno/entidade/Student.java
@@ -0,0 +1,21 @@
+package notaAluno.entidade;
+
+public class Student {
+
+    public String nome;
+    public String resultado;
+
+    public double  somaSimestre(double nota1, double nota2, double nota3) {
+
+        double resultado = nota1 + nota2 + nota3;
+        if (resultado >= 60 && resultado <= 100) {
+            System.out.print("Passou.  RESULTADO FINAL: ");
+        } else if (resultado >= 0 && resultado <= 59) {
+            System.out.println("Reprovado");
+            System.out.printf("Pontos faltantes: %.2f%n", 60 - resultado);
+            System.out.printf("Nota final: ");
+        } else {
+            System.out.println("Nota invalida.");
+        } return resultado;
+    }
+}
